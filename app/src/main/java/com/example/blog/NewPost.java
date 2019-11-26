624e6ded95cfdbab8093381752200902b9103350
zoedekker <zoeekedekker@gmail.com>
2019-11-26 14:19:56 +0100

    ik heb het terugveranderd

---
+++ app/src/main/java/com/example/blog/NewPost.java
@@ -0,0 +1,62 @@
+package com.example.blog;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.NumberPicker;
+import android.widget.Toast;
+
+public class NewPost extends AppCompatActivity {
+    private EditText editTextTitle;
+    private EditText editTextInhoud;
+    private NumberPicker numberPicker;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_post);
+
+        editTextTitle = findViewById(R.id.edit_title);
+        editTextInhoud = findViewById(R.id.edit_inhoud);
+        numberPicker = findViewById(R.id.number_picker);
+
+        numberPicker.setMinValue(1);
+        numberPicker.setMaxValue(10);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.new_post, menu);
+        return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.post:
+            saveNote();
+            return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void saveNote() {
+        String title = editTextTitle.getText().toString();
+        String inhoud = editTextInhoud.getText().toString();
+        int datum = numberPicker.getValue();
+
+        if (title.trim().isEmpty() || inhoud.trim().isEmpty()) {
+            Toast.makeText(this, "Please fill in a title and content", Toast.LENGTH_SHORT).show();
+            return;
+        }
+    }
+
+}
